ad75dc5daa7a61b8c6c98296791e24d90a92747b
ieunjung <euju718@gmail.com>
2020-03-29 00:21:23 +0900

    힙 - 더 맵게

--- programmers/heap/sol01.js
+++ programmers/heap/sol01.js
@@ -8,33 +8,26 @@
 function solution(scoville, k) {
     let answer = 0; // 모든 음식의 스코빌 지수를 K 이상으로 만들기 위해 섞어야 하는 최소 횟수
 
-    const tempScoville = new Array([scoville]);
-    while(scoville.some(scovi => scovi >= k)){
+    let tempScoville = [...scoville];
+    while(scoville.some(scovi => scovi <= k)){
         const minScovi = Math.min(...scoville);
         const idx = scoville.indexOf(minScovi);
+        
         tempScoville.splice(idx, 1);
         const secMinScovi = Math.min(...tempScoville);
-        const secIdx = scoville.indexOf(secMinScovi);
 
+        const newEl = minScovi + (secMinScovi * 2);
         
+        scoville[idx] = newEl;
 
-        console.log(minScovi, secMinScovi, scoville);
-        
+        tempScoville = [...scoville];
+
+        answer += 1;
     }
-
-    scoville.map((scovi, idx) => {
-        const prevScovi = scoville[idx - 1];
-        // console.log(scovi, prevScovi);
-        let mixed = 0;
-
-        // if(scovi < k){
-        //     if(covi > prevScovi){
-        //         mixed = prevScovi + (scovi * 2)
-        //     }else{
-        //         mixed = 
-        //     }
-        // }
-    })
+    // [5,2,3,9,10,12]
+    // [5,8,3,9,10,12]
+    // [5,8,13,9,10,12]
+    // [21,8,13,9,10,12]
     
     return answer;  
 }
