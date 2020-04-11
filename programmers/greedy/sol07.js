cde5ad54569d3dbed9a7dad25ded05628b38c1cc
ieunjung <euju718@gmail.com>
2020-04-12 03:34:01 +0900

    5회차-저울

---
+++ programmers/greedy/sol07.js
@@ -0,0 +1,22 @@
+/**
+ * 저울
+ * 모든 추 무게의 총합 + 1 = 모든 추로 측정할 수 없는 최소 무게
+ * 
+ * @param {number[]} weight 
+ */
+function solution(weight) {
+    let answer = 1; // 추의 최소 무게
+
+    weight.sort((a, b) => a - b);
+    weight.map(w => {
+        if(answer >= w){
+            answer += w;
+        }
+    });
+
+    console.log(answer);
+
+    return answer;
+}
+
+solution([3, 1, 6, 2, 7, 30, 1]);   // 21
