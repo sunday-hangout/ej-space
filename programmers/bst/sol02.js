167df52e1b650cfaa0588ac04c45a9c6c7bedc2c
ieunjung <euju718@gmail.com>
2020-05-24 04:41:34 +0900

    야근지수

--- programmers/bst/sol02.js
+++ programmers/bst/sol02.js
@@ -1,5 +1,6 @@
 /**
  * 입국심사
+ * 모든 사람이 최소의 시간으로 심사를 받게 하라
  * 
  * @param {number} n 입국심사를 기다리는 사람
  * @param {number[]} times 심사관 배열
@@ -13,18 +14,25 @@ var floor = n => Math.floor(n);
 function s(n, times) {
     var min = 0, max = n * Math.max(...times);
     // max : 느린 심사관이 모든 사람들을 심사하는 경우
+    // min = 0, max = 60;
 
     while (min <= max) {
         var mid = floor((min + max) / 2);
-        var maxInMid = times.reduce((acc, cur) => acc += floor(mid / cur), 0);
-        if (n <= maxInMid) {
+        var sum = times.reduce((acc, cur) => acc += floor(mid / cur), 0);
+        // mid / cur : 심사관 한 명당 맡을 수 있는 입국자 수
+
+        if (n <= sum) {
             max = mid - 1;
         } else {
             min = mid + 1;
         }
+        
+        console.log(mid, sum, min, max);
+        
     }
 
     return min;
 }
 
 
+solution(6, [7, 10]);
---
+++ programmers/others/야근지수.js
@@ -0,0 +1,51 @@
+/**
+ * 야근 지수
+ * 야근 피로도 = 남은 일 * 남은 일의 작업량^2
+ * N시간 동안 야근 피로도를 최소화한 값을 구하라.
+ * 
+ * @param {number} n 퇴근까지 남은 N시간
+ * @param {number[]} works 일에 대한 작업량
+ */
+function solution(n, works) {
+    let answer = 0;
+
+    const length = works.length - 1;
+
+    const sum = works.reduce((prev, curr) => prev + curr, 0);
+    // 야근할 필요가 없는 경우
+    if (n >= sum) {
+        return answer;
+    }
+
+    for (let i = 0; i < n; i++) {
+        works = works.sort((a, b) => a - b);
+        works[length] -= 1;
+        
+        console.log(i, works);
+
+        //같은 작업량 한 번에 처리하기
+        for (let j = length - 1; j >= 0; j--) {
+            if(works[length] === works[j]){
+                i += 1;
+                if(i === n){
+                    break;
+                }
+                works[j] -= 1;
+            }
+        }
+
+    }
+    
+    answer = works.reduce((prev, curr) => prev + curr * curr, answer);
+
+    console.log('답 :' + answer, works);
+
+    return answer;
+}
+
+// solution(4, [4, 3, 3]); // 12
+// solution(1, [2, 1, 2]); // 6
+// solution(3, [1, 1]); // 0
+// solution(5, [2,3,3,4]);
+solution(15, [11,5,3,2,1]); // 11, [1,1,2,2,1]
+// solution(6, [2,2,2]);   // 0
